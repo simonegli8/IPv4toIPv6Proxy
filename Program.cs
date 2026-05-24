bf867f4bbe2bee46710411d03e371b676471ec4b
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2026-05-24 23:52:26 +0200

    Initial work

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IPv4toIPv6Proxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
